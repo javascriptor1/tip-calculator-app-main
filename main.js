88f2d98832df1af71c10c3d2facd90c49e69f5e3
Mohammed Fakih <mhd.fakih@gmail.com>
2023-11-25 13:50:58 +0300

    started js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const inputAmount = document.getElementById("bill");
+const numberOfPeople = document.getElementById("number-of-people");
+const tipAmount = document.querySelector(".amount-person");
+const totalAmount = document.querySelector(".total-person");
+const resetButton = document.querySelector(".reset-btn");
+
+inputAmount.addEventListener("input", calculateTip);
+numberOfPeople.addEventListener("input", calculateTip);
+
+function calculateTip() {
+  const Allpercentage = document.querySelectorAll(
+    'input[ readonly="readonly"]'
+  );
+  let percentage;
+  Allpercentage.forEach((percentageValue) => {
+    console.log(percentageValue.activeElement);
+  });
+}
